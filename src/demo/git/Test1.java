c97b4643cbf0e476d9d35f428a54cfb1921c38c8
UdayKiranKalicheti <UdayKiranKalicheti@UdayKiran-AG>
2024-02-29 17:40:17 +0530

    First commit....

---
+++ src/demo/git/Test1.java
@@ -0,0 +1,13 @@
+package demo.git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String game = "Batminton";
+		if(game == "Batminton")
+			System.out.println("Playing game");
+
+	}
+
+}
