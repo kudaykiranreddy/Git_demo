7934c5369790b365e98449e3ff77ebf0f1721e8f
UdayKiranKalicheti <UdayKiranKalicheti@UdayKiran-AG>
2024-02-29 20:18:05 +0530

    third commit...

--- src/demo/git/Test2.java
+++ src/demo/git/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String game = "Cricket";
-		if(game == "Cricket")
+		if(game == "Cricket" && game != "Crickets")
 			System.out.println("Playing game");
 
 	}
