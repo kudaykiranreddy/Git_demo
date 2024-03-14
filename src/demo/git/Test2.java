c25ee9a98c555838e9a9b21083d481a3e4f37561
kudaykiranreddy <kudaykiranreddy143@gmail.com>
2024-03-14 21:06:03 +0530

    Fourth commit....

--- src/demo/git/Test2.java
+++ src/demo/git/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String game = "Cricket";
-		if(game == "Cricket" && game != "Crickets")
+		if(game == "Cricket" && game != "Cricketers")
 			System.out.println("Playing game");
 
 	}
